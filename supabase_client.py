6e3119b52dc16ce208c3ba7a051d584de9f14e0d
skzeeshan365 <skzeeshan3650@gmail.com>
2026-04-30 16:17:10 +0530

    updated auth and predict api

--- supabase_client.py
+++ supabase_client.py
@@ -4,7 +4,7 @@ from supabase import create_client
 
 load_dotenv()
 
-SUPABASE_URL = os.environ["SUPABASE_URL"]
+SUPABASE_URL = 'https://bpcvatwasmuunonabhuo.supabase.co'
 SUPABASE_KEY = os.environ["SUPABASE_KEY"]
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
